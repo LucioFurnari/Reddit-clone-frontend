2921b16adf68ce3158649ab209e7457481b55821
LucioFurnari <luciofurnari99@gmail.com>
2024-11-27 19:59:23 -0300

    feat: add form and fetch for signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,46 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  
+  async function handleSignup(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/auth/signup", {
+        email,
+        username,
+        password
+      });
+      console.log(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignup}>
+      <input 
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input 
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input 
+        type="text"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </form>
+  )
 }
