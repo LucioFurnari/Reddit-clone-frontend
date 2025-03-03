6e0aa0912ee4f3ba3a34bc9ad77afc77bf05d95a
LucioFurnari <luciofurnari99@gmail.com>
2025-03-03 16:58:33 -0300

    feat: change state names

--- src/components/LoginPage/LoginForm.tsx
+++ src/components/LoginPage/LoginForm.tsx
@@ -4,22 +4,24 @@ import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
-  const [username, setUsername] = useState("");
+  const [email, setUsername] = useState("");
   const [password, setPasswrord] = useState("");
-  const { login } = useAuthStore();
+  const { fetchUser } = useAuthStore();
   const router = useRouter();
 
   async function handleLogin(e: React.FormEvent) {
     e.preventDefault();
 
     try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const res = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/api/login`,
-        { username, password },
+        { email, password },
         { withCredentials: true }
       );
-
-      login(res.data.user);
+      console.log(res.data)
+      // Fetch user data after successful login
+      await fetchUser();
       router.push("/");
     } catch (error) {
       console.error("Login failed", error);
@@ -30,9 +32,9 @@ export default function LoginForm() {
     <form className="p-6 bg-gray-800 text-white rounded-lg" onSubmit={handleLogin}>
       <h2 className="text-2xl">Login</h2>
       <input
-        type="text"
+        type="email"
         placeholder="Username"
-        value={username}
+        value={email}
         onChange={(e) => setUsername(e.target.value)}
         className="block w-full p-2 my-2 text-black"
       />
