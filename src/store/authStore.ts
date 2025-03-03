0af0095665d974f773a3fabf515e0f347e6f22fb
LucioFurnari <luciofurnari99@gmail.com>
2025-03-03 15:49:41 -0300

    feat: change auth store to fetch user

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,13 +1,24 @@
+import axios from "axios";
 import { create } from "zustand";
 
 interface AuthState {
-  user: { id: string, username: string } | null;
-  login: (user: { id: string, username: string }) => void;
+  user: { id: string, username: string, email: string, createdAt: Date } | null;
+  fetchUser: () => void;
   logout: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
-  login: (user) => set({ user}),
+
+  fetchUser: async () => {
+    try {
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/user`, { withCredentials: true });
+      console.log(res.data)
+      set({ user: res.data });
+    } catch {
+      set({ user: null });
+    }
+  },
+
   logout: () => set({ user: null}),
 }));
