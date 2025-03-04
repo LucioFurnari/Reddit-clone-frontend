d81ae0e82a7ccd9a1f9210e1406c128d6e1072d3
LucioFurnari <luciofurnari99@gmail.com>
2025-03-04 20:37:07 -0300

    feat: change schema

--- src/components/LoginPage/loginSchema.ts
+++ src/components/LoginPage/loginSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.string().trim().email("The email is not valid"),
+  email: z.string().trim().min(1, "Email is required").email("The email is not valid").min(5, "The email must be at least 5 characters long"),
   password: z.string().trim().min(6, "The password must be at least 6 characters long"),
 });
