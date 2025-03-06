890e8282ba28bde1f3b5b7f5cb758c429ee06a98
LucioFurnari <luciofurnari99@gmail.com>
2025-03-06 20:10:15 -0300

    feat: add signup form

--- src/components/SignupPage/SignupForm.tsx
+++ src/components/SignupPage/SignupForm.tsx
@@ -1,9 +1,75 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signupSchema } from "./signupSchema";
+import ErrorText from "../common/ErrorText";
+import axios from "axios";
 
 
 export default function SignupForm() {
+  const [formData, setFormData] = useState({ email: "", username: "", password: "", confirmPassword: "" });
+  const [errors, setErrors] = useState<{ email?: string, username?: string, password?: string, confirmPassword?: string, server?: string}>({});
+  const router = useRouter();
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  async function handleSignup(e: React.FormEvent) {
+    e.preventDefault();
+    setErrors({});
+
+    const result = signupSchema.safeParse(formData);
+    if (!result.success) {
+      const fieldErrors = result.error.flatten().fieldErrors;
+      setErrors({ 
+        email: fieldErrors.email?.[0],
+        username: fieldErrors.username?.[0],
+        password: fieldErrors.password?.[0],
+        confirmPassword: fieldErrors.confirmPassword?.[0],
+      })
+    }
+
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/signup`,
+        {
+          email: formData.email,
+          username: formData.username,
+          password: formData.password
+        },
+      );
+
+      router.push("/login");
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.data?.error) {
+        setErrors({ email: error.response.data.error.email, username: error.response.data.error.username, password: error.response.data.error.password, confirmPassword: error.response.data.error.confirmPassword, server: error.response.data.error });
+      }
+  };
+  };
   return (
-    <form>
-      
+    <form onSubmit={handleSignup}>
+      <input
+        name="email"
+        type="email"
+        placeholder="Email"
+        value={formData.email}
+        onAbort={handleChange}
+        className="block w-full p-2 my-4"
+      />
+      <ErrorText message={errors.email} />
+      <input
+        name="username"
+        type="text"
+        placeholder="Username"
+        value={formData.username}
+        onChange={handleChange}
+        className="block w-full p-2 my-4"
+      />
+      <ErrorText message={errors.username} />
+      <button type="submit" className="bg-blue-500 my-4 p-2 rounded w-full font-semibold">Signup</button>
+      <ErrorText message={errors.server} />
     </form>
   )
-}
+};
