1b771647da8272f4c19c3dabaaef5ce2998a3432
LucioFurnari <luciofurnari99@gmail.com>
2025-03-12 23:50:32 -0300

    feat: add PostSubreddit component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,10 +1,15 @@
 import { PostInterface } from "@/interfaces"
+import PostSubreddit from "./PostSubreddit"
 
 export default function Post({post}: { post: PostInterface}) {
+  const { title, content } = post;
+  const { name, iconUrl } = post.subreddit;
+
   return (
-    <div className="flex flex-col w-full">
-      <h2 className="text-2xl text-left font-semibold text-white">{post.title}</h2>
-      <p className="text-slate-300">{post.content}</p>
+    <div className="flex flex-col w-full px-4 py-2 border-t-[1px] border-gray-400/30">
+      <PostSubreddit name={name} iconUrl={iconUrl} />
+      <h2 className="text-2xl text-left font-semibold text-white">{title}</h2>
+      <p className="text-slate-300">{content}</p>
     </div>
   )
 }
