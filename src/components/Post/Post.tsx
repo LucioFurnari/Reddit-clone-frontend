13814078ca4b4e5c90de3011c5347a60c8136957
LucioFurnari <luciofurnari99@gmail.com>
2025-03-13 23:23:20 -0300

    feat: add PostCommenCount component and date-fns function

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,17 +1,22 @@
 import { PostInterface } from "@/interfaces"
+import { formatDistanceToNow } from "date-fns";
 import PostSubreddit from "./PostSubreddit"
 import PostKarma from "./PostKarma";
+import PostCommentsCount from "./PostCommentsCount";
 
 export default function Post({post}: { post: PostInterface}) {
-  const { title, content, karma } = post;
+  const { title, content, karma, createdAt } = post;
+  const { comments } = post._count;
   const { name, iconUrl } = post.subreddit;
 
   return (
-    <div className="flex flex-col w-full px-4 py-2 border-t-[1px] border-gray-400/30">
+    <div className="w-full px-4 py-2 border-t-[1px] border-gray-400/30">
       <PostSubreddit name={name} iconUrl={iconUrl} />
-      <h2 className="text-2xl text-left font-semibold text-white">{title}</h2>
-      <p className="text-slate-300">{content}</p>
+      <span>{formatDistanceToNow(new Date(createdAt), { addSuffix: true })}</span>
+      <h2 className="text-2xl mb-2 text-left font-semibold text-white">{title}</h2>
+      <p className="text-slate-300 mb-2">{content}</p>
       <PostKarma karma={karma} />
+      <PostCommentsCount commentsCount={comments}  />
     </div>
   )
 }
