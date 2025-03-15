4b8066c0a5fd19b2dccf04773100d909cc1ba930
LucioFurnari <luciofurnari99@gmail.com>
2025-03-14 22:32:03 -0300

    feat: change in class

--- src/components/Post/PostKarma.tsx
+++ src/components/Post/PostKarma.tsx
@@ -6,14 +6,16 @@ interface PostKarmaProps {
 
 export default function PostKarma({ karma }: PostKarmaProps) {
   return (
-    <div className="inline-block">
-      <button>
-        <ArrowBigUp strokeWidth={1.5} />
-      </button>
-      <span>{karma}</span>
-      <button>
-        <ArrowBigDown strokeWidth={1.5} />
-      </button>
+    <div className="inline-block mr-2">
+      <div className="flex items-center bg-slate-400/50 py-1 rounded-3xl">
+        <button className="px-1">
+          <ArrowBigUp strokeWidth={1.5} size={20} />
+        </button>
+        <span>{karma}</span>
+        <button className="px-1">
+          <ArrowBigDown strokeWidth={1.5} size={20}/>
+        </button>
+      </div>
     </div>
   )
 }
