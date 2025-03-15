239b133ac405565411dfeefb63c700d82ef24a04
LucioFurnari <luciofurnari99@gmail.com>
2025-03-15 01:21:42 -0300

    feat: improve style

--- src/components/Post/PostKarma.tsx
+++ src/components/Post/PostKarma.tsx
@@ -7,12 +7,12 @@ interface PostKarmaProps {
 export default function PostKarma({ karma }: PostKarmaProps) {
   return (
     <div className="inline-block mr-2">
-      <div className="flex items-center bg-slate-400/50 py-1 rounded-3xl">
-        <button className="px-1">
+      <div className="flex items-center bg-slate-400/50 rounded-3xl">
+        <button className="px-1 py-1 hover:bg-slate-400/30 hover:text-orange-600 rounded-3xl">
           <ArrowBigUp strokeWidth={1.5} size={20} />
         </button>
         <span>{karma}</span>
-        <button className="px-1">
+        <button className="px-1 py-1 hover:bg-slate-400/30 hover:text-blue-500 rounded-3xl">
           <ArrowBigDown strokeWidth={1.5} size={20}/>
         </button>
       </div>
