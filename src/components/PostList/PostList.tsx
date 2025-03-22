42f6769ce7c0cf530d4c1fc03bebcea00d7c46a3
LucioFurnari <luciofurnari99@gmail.com>
2025-03-21 23:57:48 -0300

    feat: change component to fetch general post or users

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,7 @@
 import { PostInterface } from "@/interfaces";
 
 import Post from "../Post/Post";
+import { cookies } from "next/headers";
 
 interface PostsListInterface {
   posts: PostInterface[] | []
@@ -8,16 +9,19 @@ interface PostsListInterface {
 
 export default async function PostList() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, {
-      cache: "no-store",
-    });
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value; 
 
-    if (!res.ok) throw new Error("Failed to fetch posts");
-
-    const data: PostsListInterface = await res.json();
-
-    return (
-      <section className="lg:mx-16">
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts/subscribed`, {
+    //   cache: "no-store",
+    // });
+    if (!token) {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, {
+        cache: "no-store",
+      });
+      const data: PostsListInterface = await res.json();
+      return (
+        <section>
         {
           data.posts.length > 0 ?
           (
@@ -26,8 +30,63 @@ export default async function PostList() {
             <p>There are no posts at the moment</p>
           )
         }
-      </section>
-    )
+        </section>
+      )
+    };
+
+    if (token) {
+      // 2️⃣ Fetch user data using the token
+      const userRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user`, {
+        headers: { Cookie: `token=${token}` },
+        cache: "no-store",
+        credentials: "include"
+      });
+      console.log(userRes.status)
+      if (userRes.ok) {
+        // 3️⃣ Fetch posts from user's subscribed subreddits
+        const postsRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts/subscribed`, {
+          cache: "no-store",
+          credentials: "include"
+        });
+        console.log(postsRes.status)
+        if (!postsRes.ok) throw new Error("Failed to fetch posts");
+
+        if (postsRes.ok) {
+          const data = await postsRes.json();
+          return (
+            <section>
+              {
+                data.posts.length > 0 ?
+                (
+                  data.posts.map((post: PostInterface) => <Post key={post.id} post={post} />)
+                ) : (
+                  <p>There are no posts at the moment</p>
+                )
+              }
+            </section>
+          )
+        }
+      } else {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, {
+        cache: "no-store",
+      });
+      const data: PostsListInterface = await res.json();
+      return (
+        <section>
+        {
+          data.posts.length > 0 ?
+          (
+            data.posts.map((post: PostInterface) => <Post key={post.id} post={post} />)
+          ) : (
+            <p>There are no posts at the moment</p>
+          )
+        }
+        </section>
+      )
+      };
+    };
+
+    // const data: PostsListInterface = await res.json()
   } catch {
     return <p>Failed to load posts. Please try again later.</p>
   }
