14f67977ff975ce19dc21ac5f4027aa99b6677ad
LucioFurnari <luciofurnari99@gmail.com>
2025-04-01 23:33:40 -0300

    feat: change function and name

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -3,8 +3,8 @@ import { create } from "zustand";
 
 interface AuthState {
   user: { id: string, username: string, email: string, createdAt: Date } | null;
-  fetchUser: () => void;
-  logout: () => void;
+  fetchUser: () => Promise<boolean>;
+  stateLogout: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
@@ -13,16 +13,17 @@ export const useAuthStore = create<AuthState>((set) => ({
   fetchUser: async () => {
     try {
       const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/user`, { withCredentials: true });
-      console.log(res.data)
+
       set({ user: res.data });
+      return true;
     } catch {
       set({ user: null });
+      return false;
     }
   },
 
-  logout: async () => {
+  stateLogout: async () => {
     try {
-      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/logout`, {}, { withCredentials: true });
       set({ user: null });
     } catch (error) {
       console.error("Error en logout:", error);
